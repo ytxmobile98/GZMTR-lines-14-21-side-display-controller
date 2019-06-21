578fb192196834006b1ec9b45190a2caadd42946
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-06-21 00:20:55 -0700

    jsx minor update

--- React/controller.jsx
+++ React/controller.jsx
@@ -14,7 +14,7 @@ class Controller extends React.Component {
 				<LED
 					showContent={true}
 					serviceType={SERVICE_TYPES["不载客"]}
-					destination={DESTINATIONS["不载客"]["不载客列车"]}
+					destination={DESTINATIONS["不载客"]["不载客"]}
 				/>
 			</div>
 		);
