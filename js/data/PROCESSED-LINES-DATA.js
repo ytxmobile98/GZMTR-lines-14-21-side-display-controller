b880b9c9350c2358d4aeb591155318f64da8de6d
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-06-22 16:02:44 -0700

    freeze processed data

--- js/data/PROCESSED-LINES-DATA.js
+++ js/data/PROCESSED-LINES-DATA.js
@@ -5,8 +5,8 @@ import { processTranslations } from "./process-translations.js";
 import { ServiceType } from "./service-type-classes.js";
 import { Station } from "./station-classes.js";
 
-const SERVICE_TYPES_TRANSLATIONS = processTranslations(RAW_SERVICE_TYPES_TRANSLATIONS, ServiceType);
+const SERVICE_TYPES_TRANSLATIONS = Object.freeze( processTranslations(RAW_SERVICE_TYPES_TRANSLATIONS, ServiceType));
 
-const DESTINATIONS_TRANSLATIONS = processTranslations(RAW_DESTINATIONS_TRANSLATIONS, Station);
+const DESTINATIONS_TRANSLATIONS = Object.freeze( processTranslations(RAW_DESTINATIONS_TRANSLATIONS, Station));
 
 console.log(SERVICE_TYPES_TRANSLATIONS, DESTINATIONS_TRANSLATIONS);
