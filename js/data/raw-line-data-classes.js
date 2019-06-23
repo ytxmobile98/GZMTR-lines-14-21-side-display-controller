858d8868a9f4b260bb3c09eeb7bffd79ce7dd04e
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-06-23 11:15:30 -0700

    moved around variables

--- js/data/raw-line-data-classes.js
+++ js/data/raw-line-data-classes.js
@@ -4,19 +4,24 @@ import { TypeChecker } from "../type-checker.js";
 import { RawFilter } from "./filter-classes.js"
 
 class RawLineInfo {
-	constructor(line, serviceTypes, defaultServiceType, crossLineServiceType, destinations, rawFilters) {
+	constructor(line, destinations, serviceTypes, defaultServiceType, crossLineServiceType, rawFilters) {
+
 		TypeChecker.checkTypeOf(line, "string");
+		TypeChecker.checkArrayType(destinations, "string");
+
 		TypeChecker.checkArrayType(serviceTypes, "string");
 		TypeChecker.checkTypeOf(defaultServiceType, "string");
 		TypeChecker.checkTypeOf(crossLineServiceType, "string");
-		TypeChecker.checkArrayType(destinations, "string");
+
 		TypeChecker.checkArrayType(rawFilters, RawFilter);
 
 		this.line = line;
+		this.destinations = destinations;
+
 		this.serviceTypes = serviceTypes;
 		this.defaultServiceType = defaultServiceType;
 		this.crossLineServiceType = crossLineServiceType;
-		this.destinations = destinations;
+
 		this.rawFiters = rawFilters;
 	}
 }
