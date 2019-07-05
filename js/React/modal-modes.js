40c652013e30166bb97d0e91b02ea841b51f3cbb
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-07-05 08:22:22 -0700

    removed that

--- js/React/modal-modes.js
+++ js/React/modal-modes.js
@@ -5,9 +5,8 @@ import { TypeChecker } from "../type-checker.js";
 const MODAL_MODE_NAMES = ["standby", "setDisplayMode", "setDestination"];
 const MODAL_MODES = Object.freeze(new function () {
 	TypeChecker.checkArrayType(MODAL_MODE_NAMES, "string");
-	const that = this;
 	MODAL_MODE_NAMES.forEach(name => {
-		that[name] = Symbol();
+		this[name] = Symbol();
 	});
 }());
 console.log(`MODAL MODES: `, MODAL_MODES);
