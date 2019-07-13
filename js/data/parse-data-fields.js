9631976111faabc5cb4ea12c083cf84bc367f9de
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-07-13 16:31:02 -0700

    parse data fields code

---
+++ js/data/parse-data-fields.js
@@ -0,0 +1,46 @@
+"use strict";
+
+import { TypeChecker } from "../type-checker.js";
+
+const getQuotedFieldData = (field) => {
+	field = String(field);
+	const re = /".*"|'.*'/g;
+	if (field.match(re)) {
+		// strip the first and last quotation marks
+		return field.slice(1, -1);
+	}
+	return field;
+}
+
+const parseDataFields = (text, fieldSeparator = "\t", newLine = "\n") => {
+	text = String(text);
+	const rows = text.split(newLine);
+	const result = rows.map((row) => {
+		return row.split(fieldSeparator).map(item => getQuotedFieldData(item));
+	});
+	return result;
+}
+
+const isEmptyRow = (arr) => {
+	TypeChecker.checkInstanceOf(arr, Array);
+	for (let i of arr) {
+		if (getQuotedFieldData(i)) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+const isHeaderRow = (arr, regexp) => {
+	TypeChecker.checkInstanceOf(arr, Array);
+	for (let i of arr) {
+		if (!(String(i).match(regexp))) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+export { parseDataFields, isEmptyRow, isHeaderRow };
--- js/data/read-raw-data.js
+++ js/data/read-raw-data.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { parseDataFields, isEmptyRow, isHeaderRow } from "./parse-data-fields.js";
+
 const makeRequest = (url, mimeType = "text/plain") => {
 	return new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
@@ -24,13 +26,24 @@ const parallelProcess = async () => {
 	await Promise.all([
 		(async () => {
 			destinations = await makeRequest("./RAW-DATA/DESTINATIONS.csv", mimeType);
+			destinations = parseDataFields(destinations);
 		})(),
 		(async () => {
 			serviceTypes = await makeRequest("./RAW-DATA/SERVICE-TYPES.csv", mimeType);
+			serviceTypes = parseDataFields(serviceTypes);
+		})(),
+		(async () => {
+			filters = await makeRequest("./RAW-DATA/LINES-FILTERS.csv", mimeType);
+			filters = parseDataFields(filters);
 		})(),
 	]);
 	console.log(destinations)
 	console.log(serviceTypes);
+	console.log(filters);
+
+	serviceTypes.forEach((row) => {
+		console.log(isHeaderRow(row, /^!.{1,}/g));
+	});
 }
 
 parallelProcess();
