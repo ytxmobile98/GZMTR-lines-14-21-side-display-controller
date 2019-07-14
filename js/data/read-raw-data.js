27ba4ad05efe353bd9d8c391e4ad47c8e92f5e39
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-07-14 16:54:22 -0700

    removed unnecessary imports and test file

--- js/data/read-raw-data.js
+++ js/data/read-raw-data.js
@@ -1,13 +1,5 @@
 "use strict";
 
-import { TypeChecker } from "../type-checker.js";
-
-import { TranslationPair } from "./translation-pairs.js";
-import { ServiceType } from "./service-type-classes.js";
-import { Station } from "./station-classes.js";
-
-import { parseDataFields, isEmptyRow, isHeaderRow } from "./parse-data-fields.js";
-
 const makeRequest = (url, mimeType = "text/plain") => {
 	return new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
