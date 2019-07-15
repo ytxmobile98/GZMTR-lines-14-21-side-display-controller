8bb685c7739e93d3f5d9a12dc337dcdfe81dfe13
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-07-15 00:51:47 -0700

    modified initial load

--- js/React/initial-load.js
+++ js/React/initial-load.js
@@ -1,12 +1,15 @@
 "use strict";
 
 import { readTranslationsData } from "../data/TRANSLATIONS-DATA.js";
+import { loadLinesInfo } from "../data/LINES-DATA.js";
+
 import { Controller } from "./controller.js";
 
 const root = document.getElementById("js-root");
 
 const initialLoad = async () => {
 	await readTranslationsData();
+	await loadLinesInfo();
 	ReactDOM.render(React.createElement(Controller, null), root);
 };
 
