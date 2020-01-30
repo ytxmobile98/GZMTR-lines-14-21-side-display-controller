4b9de597aa7bdb1f39a9cdca998b087975874082
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2020-01-30 09:05:45 -0800

    changed order of command-line arguments

--- jsx-compile.sh
+++ jsx-compile.sh
@@ -1,33 +1,33 @@
-echo -e "Usage: sh jsx-compile.sh [extension [outputDir [watchDir]]]\n"
+echo -e "Usage: sh jsx-compile.sh [outputDir [watchDir [extension]]]\n"
 
 echo -e "NOTE: If you have not yet initialized the jsx compile environment, please run the following commands first:"
 echo -e "npm init -y"
 echo -e "npm install --save-dev @babel/cli @babel/preset-react"
 echo -e "See: https://reactjs.org/docs/add-react-to-a-website.html"
 
-ext=".jsx"
 outputDir="./js/React"
 watchDir="./jsx"
+ext=".jsx"
 
 if [ -n "$1" ]
 then
-	ext="$1"
+	outputDir="$1"
 fi
-
 
 if [ -n "$2" ]
 then
-	outputDir="$2"
+	watchDir="$2"
 fi
 
 if [ -n "$3" ]
 then
-	watchDir="$3"
+	ext="$3"
 fi
 
-echo -e "\n[File extension]\n$ext\n"
+echo ""
+echo -e "[File extension]\n$ext\n"
+echo -e "[Watch directory] (where you should have your $ext files)\n$(realpath "$watchDir")\n"
 echo -e "[Output directory] (where the compiled js files are located)\n$(realpath "$outputDir")\n"
-echo -e "[Watch directory] (where you should have $ext files)\n$(realpath "$watchDir")\n"
 
 read -p "Continue? (y/n): " -r reply
 # See: https://thoughtbot.com/blog/the-unix-shells-humble-if
